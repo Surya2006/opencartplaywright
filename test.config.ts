a94e46a602a57599d434b7db5bdab82935d0a401
Swathi <swathiprasadb@gamil.com>
2025-12-16 14:14:56 -0500

    my first commit

---
+++ test.config.ts
@@ -0,0 +1,20 @@
+export class TestConfig{ 
+    
+    //appUrl="http://localhost/opencart/upload/" 
+
+    //appurl="https:tutorialsninja.com/demo/"
+  appurl="https://naveenautomationlabs.com/opencart"
+
+       
+    //valid login credentials- create your own login account 
+    //email="testbs@gmail.com" 
+    //password="testsb" 
+ 
+    email="testap@gmail.com" 
+    password="testap123" 
+
+    //product details     
+    productName="MacBook"     
+    productQuantity="2" 
+    totalPrice="$1,204.00" 
+} 
